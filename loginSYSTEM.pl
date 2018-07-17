d860d719a8888a1bdde909540aa6b00dd3d2eec9
voicu cosmin <voicu_cosmin95@yahoo.com>
2018-07-17 23:21:54 +0300

    loginsystem_v1.1

--- loginSYSTEM.pl
+++ loginSYSTEM.pl
@@ -1,26 +1,31 @@
 use strict;
 use warnings;
-print "Introdu nume utilizator: "; my $u =<STDIN>; print "\n";
-print "Introdu parola: "; my $p =<STDIN>; print "\n";
+print "Introdu nume utilizator: "; my $u =<STDIN>; 
+print "Introdu parola: "; my $p =<STDIN>;
 
 my $filename = '/Users/voicucosmin/Desktop/perl/logins.txt';
 open(my $linie,'<',$filename)
    or die "Fisierul nu a putut fi deschis '$filename' $!";
-my $linia1 = <$linie>;
-my $linie2 = <$linie>;
+while(my $linia1=<$linie>)
+{
+chomp $linia1;
 $linia1 =~ tr/[/ /;
 $linia1 =~ tr/]/ /;
+   
 my ($user,$parola) = split(' ',$linia1);
 
 if($u =~ $user and $p =~ $parola)
 {
     print "Poti utiliza functionalitatea 2\n";
+    last;
 }
 else
 {
     print "Ai intordus un nume de utlizator sau parola gresita\n";
 }
-
+}
     
 
+# ^[a-zA-Z][a-zA-Z0-9_]*$                     #username
+# (?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{6,}$  #parola
 
